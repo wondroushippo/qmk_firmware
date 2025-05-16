8fe5fc28eb3488c64820615d406e8e0103fb1531
wondroushippo <wondroushippo@gmail.com>
2025-05-16 16:00:23 -0500

    removing rp2040 reset stuff

--- keyboards/sofle/keymaps/holykeebs/config.h
+++ keyboards/sofle/keymaps/holykeebs/config.h
@@ -41,11 +41,6 @@
 //
 #define RGBLIGHT_LAYERS
 
-
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
-
 /* ws2812 RGB LED */
 #define WS2812_DI_PIN D3
 
